4589c70ea02c18355d19b57460cd2f796d331146
d0ubt <sepabonn@unal.edu.co>
2025-02-10 18:56:28 -0500

    Cambios en GestorDB.py y usuario.py

--- GestorDB.py
+++ GestorDB.py
@@ -1,9 +1,14 @@
 import sqlite3
+import time
 
 class GestorDB:
-    def __init__(self):
-        self.conexion = None
-        self.cursor = None
+    def __init__(self, path_db = 'fumiservicios.db'):
+        self.path_db = path_db
+        self.conexion = sqlite3.connect(path_db)
+        self.conexion.row_factory = sqlite3.Row
+        self.cursor = self.conexion.cursor()
+        self.cursor.execute("PRAGMA foreign_keys = ON;")
+        
 
     def abrir_conexion(self, path_db = 'fumiservicios.db', as_dict = False):
         """Abre conexion con la base de datos usando sqlite3
@@ -20,6 +25,24 @@ class GestorDB:
 
         self.cursor = self.conexion.cursor()
         self.cursor.execute("PRAGMA foreign_keys = ON;")
+    
+    def ver_usuarios(self,limit: int):
+        self.cursor.execute("SELECT * FROM USUARIO LIMIT ?",(limit, ))
+        return self.cursor.fetchall()
+    
+    def ver_usuario(self, id: int):
+        self.cursor.execute("SELECT * FROM USUARIO WHERE ID = ? ", (id,))
+        return self.cursor.fetchone()
+
+    def agregar_usuario(self,usuario):
+        try:
+            with sqlite3.connect(self.path_db) as conexion:
+                cursor = conexion.cursor()
+                cursor.execute("INSERT INTO USUARIO (id,nombre,email,contrasena,tipo) VALUES (?,?,?,?,?)",(usuario.id,usuario.nombre,usuario.email,usuario.contrasena,usuario.tipo))
+                time.sleep(20)
+                conexion.commit()
+        except sqlite3.IntegrityError:
+            raise ValueError("ID o Email ya existen")
 
     def cerrar_conexion(self):
         if self.conexion:
--- routers/usuario.py
+++ routers/usuario.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter ,HTTPException
-import sqlite3
 from pydantic import BaseModel
+from GestorDB import GestorDB
+
 
 class Usuario(BaseModel):
     id: int
@@ -9,41 +10,23 @@ class Usuario(BaseModel):
     contrasena: str
     tipo: str
 
-class GestorDB:
-    def __init__(self, path_db = 'fumiservicios.db'):
-        self.path_db = path_db
-        self.conexion = sqlite3.connect(path_db)
-        self.cursor = self.conexion.cursor()
-        self.cursor.execute("PRAGMA foreign_keys = ON;")
-
-    def ver_usuarios(self,limit: int):
-        self.cursor.execute("SELECT * FROM USUARIO LIMIT ?",(limit, ))
-        return self.cursor.fetchall()
-
-    def agregar_usuario(self,usuario: Usuario):
-        try:
-            self.cursor.execute("INSERT INTO USUARIO (id,nombre,email,contrasena,tipo) VALUES (?,?,?,?,?)",(usuario.id,usuario.nombre,usuario.email,usuario.contrasena,usuario.tipo))
-            self.conexion.commit()
-        except sqlite3.IntegrityError:
-            raise ValueError("ID o Email ya existen")
-
-        
 db = GestorDB()
 router = APIRouter()
 
 #Ver los primeros 10 o limit usuarios
 @router.get('/usuarios')
-async def users(limit: int = 10):
+async def obtener_usuarios(limit: int = 10):
     usuarios = db.ver_usuarios(limit)
-    usuarios_json = [Usuario(id = usuario[0],nombre = usuario[1],email = usuario[2], contrasena = usuario[3],tipo = usuario[4]) for usuario in usuarios]
-    return usuarios_json
+    if not usuarios:
+        return {'message': 'No hay usuarios'}
+    return usuarios
 
 
 #Agregar usuario
 @router.post('/usuario')
-async def users(user:Usuario):
+async def agregar_usuario(usuario:Usuario):
     try:
-        db.agregar_usuario(user)
+        db.agregar_usuario(usuario)
         return {'message': 'Usuario creado correctamente'}
     except ValueError as error:
         raise HTTPException(400,str(error))
@@ -51,14 +34,11 @@ async def users(user:Usuario):
 
 #Ver un usuario en especifico
 @router.get('/usuario/{id}')
-async def users(id:int):
-    db.cursor.execute("SELECT * FROM USUARIO WHERE ID = ? ", (id,))
-    usuario = db.cursor.fetchone()
+async def obtener_usuario(id:int):
+    usuario = db.ver_usuario(id)
     if not usuario:
         raise HTTPException(400, 'Usuario no encontrado')
-
-    return Usuario(id = usuario[0],nombre = usuario[1],email = usuario[2], contrasena = usuario[3],tipo = usuario[4])
-
+    return usuario
     
 
 
