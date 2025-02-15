346c34a5c71ab68bd6385963d435c7d8c0f7ee05
d0ubt <sepabonn@unal.edu.co>
2025-02-15 18:48:25 -0500

    Agregando agenda

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import usuario, solicitud, cliente
+from routers import usuario, solicitud, cliente, agenda
 
 app = FastAPI()
 
 app.include_router(usuario.router)
 app.include_router(solicitud.router)
 app.include_router(cliente.router)
+app.include_router(agenda.router)
 
 @app.get('/')
 async def hola():
---
+++ routers/agenda.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from pydantic import BaseModel, Field
+from GestorDB import GestorDB
+from typing import Literal
+from datetime import date
+
+class Solicitud(BaseModel):
+    id : int = Field(gt = 0)
+    id_usuario : int = Field(gt = 0)
+    fecha : date
+    tipo_actividad : str = Literal['Diagnostico', 'Servicio']
+    
+db = GestorDB()
+router = APIRouter(prefix='/agenda')
