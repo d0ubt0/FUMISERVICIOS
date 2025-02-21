79d2acd00e4ac81aa82043899e1064f047003902
d0ubt <sepabonn@unal.edu.co>
2025-02-20 21:57:54 -0500

    Agregando middleware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routers import usuario, solicitud, cliente
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(usuario.router)
@@ -10,3 +12,16 @@ app.include_router(cliente.router)
 @app.get('/')
 async def hola():
     return 'hola'
+
+origins = [
+    
+    'http://localhost:2999'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
